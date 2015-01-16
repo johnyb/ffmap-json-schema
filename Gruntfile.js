80aed49327e73df0f9af6764f1010d805dfeb556
Julian Bäume <julian@svg4all.de>
2015-01-16 20:13:06 +0100

    add a first simple example

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,17 @@
 module.exports = function (grunt) {
     grunt.config.init({
         tv4: {
-            options: {
-                root: grunt.file.readJSON('schema/schema_draft4.json')
-            },
             ffmap_schema: {
+                options: {
+                    root: grunt.file.readJSON('schema/schema_draft4.json')
+                },
                 src: ['schema/ffmap.json']
+            },
+            examples: {
+                options: {
+                    root: grunt.file.readJSON('schema/ffmap.json')
+                },
+                src: ['examples/*.json']
             }
         }
     });
